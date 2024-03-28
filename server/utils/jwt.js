b306079268b6fde9ce5c1a9ee4f7c32a7ffc4eec
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-28 18:54:19 +0530

    refactor logout functionality

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -12,17 +12,17 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
   const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
 
   const oneDay = 1000 * 60 * 60 * 24;
-
+  const longerExp = 1000 * 60 * 60 * 24 * 30;
   res.cookie("accessToken", accessTokenJWT, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     signed: true,
-    maxAge: 1000 * 5,
+    expires: new Date(Date.now() + oneDay),
   });
 
   res.cookie("refreshToken", refreshTokenJWT, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
+    expires: new Date(Date.now() + longerExp),
     secure: process.env.NODE_ENV === "production",
     signed: true,
   });
