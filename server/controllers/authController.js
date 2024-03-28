3920166280bfa159a54b9abe18bddc204bb2c448
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-28 17:07:28 +0530

    send verification email functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,8 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const { attachCookiesToResponse, createTokenUser } = require("../utils");
+const { attachCookiesToResponse, createTokenUser, sendVerificationEmail } = require("../utils");
 const crypto = require("crypto");
-const sendEmail = require("../utils/sendEmail");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -26,7 +25,8 @@ const register = async (req, res) => {
     role,
     verificationToken,
   });
-  await sendEmail();
+  
+  await sendVerificationEmail({name: user.name, email: user.email, verificationToken: user.verificationToken, origin: 'http://localhost:3000'});
   res.status(StatusCodes.CREATED).json({
     msg: "Success! Please check your email to verify account",
   });
